88a06a5cfa52e91eaa5f1a5995c21e9e3bde0253
whit <armstrong.whit@gmail.com>
2009-09-15 09:03:13 -0400

    added Matplot.plot(M) to run model scripts

--- run_radon_varying_intercept.py
+++ run_radon_varying_intercept.py
@@ -1,6 +1,7 @@
 import pymc
 import radon_varying_intercept
 from pylab import hist, show
+from pymc import Matplot
 
 M = pymc.MCMC(radon_varying_intercept)
 M.sample(iter=50e3, burn=10e3, thin=5)
@@ -8,3 +9,5 @@ M.sample(iter=50e3, burn=10e3, thin=5)
 fit = M.stats()
 for k in fit.keys():
      print(k,fit[k]['mean'])
+
+Matplot.plot(M)
--- run_radon_varying_intercept_and_slope.py
+++ run_radon_varying_intercept_and_slope.py
@@ -1,6 +1,7 @@
 import pymc
 import radon_varying_intercept_and_slope
 from pylab import hist, show
+from pymc import Matplot
 
 M = pymc.MCMC(radon_varying_intercept_and_slope)
 M.sample(iter=50e3, burn=10e3, thin=10)
@@ -8,3 +9,5 @@ M.sample(iter=50e3, burn=10e3, thin=10)
 fit = M.stats()
 for k in fit.keys():
      print(k,fit[k]['mean'])
+
+Matplot.plot(M)
